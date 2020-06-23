1d69a01ebdcbea5bf0814a2c1385c3850eb2dedf
Thang <pavelpro84@gmail.com>
2020-06-23 10:23:24 +0700

    Add danhsachhangxe

--- WebApplication2/WebApplication2/Areas/Admin/Controllers/HomeController.cs
+++ WebApplication2/WebApplication2/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +15,12 @@ namespace WebApplication2.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult danh_sach_hang_xe(string search)
+        {
+
+            List<hang_xe> li_hang_xe = db.Database.SqlQuery<hang_xe>("tim_kiem_hang_xe @search", new SqlParameter("@search", search ?? (object)DBNull.Value)).ToList();
+            return View(li_hang_xe);
         }
     }
 }
