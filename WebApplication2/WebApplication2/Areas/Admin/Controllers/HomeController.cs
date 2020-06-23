b3415a224d39404eb69537bf6ba955d2b576473a
Du <du99cy@gmail.com>
2020-06-23 09:33:01 +0700

    create layout admin

---
+++ WebApplication2/WebApplication2/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Database;
+
+namespace WebApplication2.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        BikeStoreShoppingEntities db = new BikeStoreShoppingEntities();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
